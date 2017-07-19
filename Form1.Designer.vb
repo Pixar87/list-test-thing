d5f8e94648c21d04926e94fb0ee5f54c5956cc3d
Tom <filipowicz78@gmail.com>
2017-07-19 19:19:28 +0200

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        resources.ApplyResources(Me.ListBox1, "ListBox1")
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.SelectionMode = System.Windows.Forms.SelectionMode.None
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 17
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Const frameRate As Short = 17 'frame delay, in ms, since 1000/60 ~ 17 (actually 16.(6))
+    Dim i As Integer
+    Dim toggle As Boolean
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = frameRate
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        i = i + 1
+
+        Try
+            ListBox1.Items.Add($"String number {i}") 'try to increment "i" by 1
+        Catch 'if we get an exception...
+            Timer1.Stop() '..stop the timer, ...
+            MsgBox($"Something went wrong while the timer was ticking...{vbCrLf}The timer has been stopped.{vbCrLf}We are sorry for the inconvenience.{vbCrLf}The application will close now.") '...show the MsgBox...
+            Application.Exit() '... and exit the application.
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click 'handles when Button1 is clicked
+        If toggle = False Then ' check if boolean "toggle" is False.
+            Timer1.Start() 'Start timer
+        Else
+            Timer1.Stop() 'Stop timer
+        End If
+        toggle = Not toggle 'invert "toggle"
+        'MsgBox($"e = {e}")
+    End Sub
+End Class
